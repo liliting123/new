45784ae288b2900c6b22a0c5620fa3c479b8b18e
zengqiuyuan <1563806584@qq.com>
2022-05-27 16:18:18 +0800

    添加全局样式和组件

---
+++ src/components/searchList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search">
+    <div class="left">
+      <slot name="left"></slot>
+    </div>
+    <div class="right">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'searchList'
+  }
+</script>
+
+<style lang="scss" scoped>
+.search {
+  width: 100%;
+  height: 100px;
+  background: white;
+  line-height: 100px;
+  margin-bottom: 15px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 15px 0 15px;
+}
+</style>
