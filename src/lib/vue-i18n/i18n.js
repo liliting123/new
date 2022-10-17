f7199a6fae78fff2d07e81f7d2db5252b47510aa
zengqiuyuan <1563806584@qq.com>
2022-10-17 16:51:15 +0800

    优化

--- src/lib/vue-i18n/i18n.js
+++ src/lib/vue-i18n/i18n.js
@@ -1,14 +1,10 @@
 // 国际化插件
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
-import cn from './cn' // 中文
-import en from './en' // 英语
+import messages from '../vue-i18n'
 Vue.use(VueI18n)
 const i18n = new VueI18n({
   locale: 'cn',
-  messages: {
-    cn,
-    en
-  }
+  messages
 })
 export default i18n
